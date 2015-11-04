12c51ac6a2ffa1072c15e39d96b5460864dbbce2
Sarah Turek <snturek@gmail.com>
2015-11-04 14:35:19 -0700

    added mongolabs

--- src/server/_config.js
+++ src/server/_config.js
@@ -2,6 +2,7 @@ var config = {};
 
 config.mongoURI = {
   development: 'mongodb://localhost/yahtzee',
+  production: 'mongodb://heroku_g97s4k7n:oksbt98emjpn5slb19667qidk@ds049094.mongolab.com:49094/heroku_g97s4k7n'
 };
 
 module.exports = config;
